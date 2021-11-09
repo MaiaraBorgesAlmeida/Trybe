3b8de58593b7221bfa0fa614770a1d525b30f96b
MaiaraBorgesAlmeida <maiaraborgesamd@gmail.com>
2021-11-09 15:40:58 -0300

    Exercicio 03

--- Fundamentos/Bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercício-03.js
+++ Fundamentos/Bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercício-03.js
@@ -1,8 +1,8 @@
 /* 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
 
-const a = 19;
+const a = 15;
 const b = 32;
-const c = 15;
+const c = 19;
 
 if (a > b && a > c){
     console.log("a é maior");
@@ -11,5 +11,5 @@ if (a > b && a > c){
     console.log("b é maior");
 
 } else {
-    console.log("c é maior")
+    console.log("c é maior");
 }
