61797f0eb1e1a6bf0e6fbb93ca913570f6dfa7c4
MaiaraBorgesAlmeida <maiaraborgesamd@gmail.com>
2021-11-09 14:50:45 -0300

    Exercicio 01

---
+++ Fundamentos/Bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercicio-01.js
@@ -0,0 +1,14 @@
+//Adição (a + b)
+//Subtração (a - b)
+//Multiplicação (a * b)
+//Divisão (a / b)
+//Módulo (a % b)
+
+const a = 32;
+const b = 19;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
